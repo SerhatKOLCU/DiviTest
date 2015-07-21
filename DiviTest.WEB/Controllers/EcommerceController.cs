bb7452f26ff531a0cb2af110ff125cd9bf6ac0c9
alphasero <serhat.kolcu@ogr.sakarya.edu.tr>
2015-07-21 15:16:03 +0300

    firt push
    sss

---
+++ DiviTest.WEB/Controllers/EcommerceController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DiviTest.WEB.Controllers
+{
+    public class EcommerceController : Controller
+    {
+
+        public ActionResult ProductsGrid()
+        {
+            return View();
+        }
+
+        public ActionResult ProductsList()
+        {
+            return View();
+        }
+
+        public ActionResult ProductEdit()
+        {
+            return View();
+        }
+
+        public ActionResult Orders()
+        {
+            return View();
+        }
+	}
+}
---
+++ DiviTest.WEB/Controllers/GalleryController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DiviTest.WEB.Controllers
+{
+    public class GalleryController : Controller
+    {
+
+        public ActionResult BasicGallery()
+        {
+            return View();
+        }
+
+        public ActionResult BootstrapCarusela()
+        {
+            return View();
+        }
+	}
+}
